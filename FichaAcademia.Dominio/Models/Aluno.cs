a4b75a7d1b6f908e876efb4b4b417c8688d358a9
Fepe Marte <felipempduarte@outlook.com>
2019-11-08 13:06:45 -0300

    Criando classes e associando ao Contexto

---
+++ FichaAcademia.Dominio/Models/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FichaAcademia.Dominio.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [StringLength(100, ErrorMessage = "Use menos caracteres!")]
+        public string NomeCompleto { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Range(14,100, ErrorMessage = "Idade inválida!")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Range(10, 150, ErrorMessage = "Peso inválida!")]
+        public double peso { get; set; }
+
+        public int ProfessorId { get; set; }
+        public Professor Professor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Range(1, 7, ErrorMessage = "Frequencia inválida!")]
+        public int FrequenciaSemanal { get; set; }
+
+        public ICollection<Ficha> Fichas { get; set; }
+    }
+}
---
+++ FichaAcademia.Dominio/Models/Ficha.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FichaAcademia.Dominio.Models
+{
+    public class Ficha
+    {
+        public int FichaId { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [StringLength(50, ErrorMessage = "Use menos caractesres!")]
+        public int Nome { get; set; }
+
+        public DateTime Cadastro { get; set; }
+        public DateTime Validade { get; set; }
+
+        public int AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+
+        public ICollection<ListaExercicio> ListaExercicios { get; set; }
+    }
+}
